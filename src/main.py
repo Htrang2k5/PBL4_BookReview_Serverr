3082a0e811c51ec799de23e38b2294ccc0cd92f7
Htrang2k5 <nguyenhuyentrang2005cx@gmail.com>
2025-12-07 21:58:36 +0700

    fix bug can't find iamges

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import authors, posts, sessions, users
 
@@ -14,3 +17,11 @@ app.include_router(users.router)
 app.include_router(authors.router)
 app.include_router(posts.router)
 app.include_router(sessions.router)
+
+# BASE_DIR = thư mục BOOKREVIEW_SERVER (project root)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# Mount static
+app.mount('/static', StaticFiles(directory=STATIC_DIR), name='static')
